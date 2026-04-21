a33a0eda9981f54687c6191fcf6fd89529a661fc
Jasir PK <jasir@tathkarah.in>
2026-04-21 21:58:50 +0530

    jsx part 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   const name = 'john';
-  return ( 
-    
-    <div className="App">
+  const a = 5;
+  const b = 10;
+  const message = () => {
+    return "Welcome to React!";
+  }
+  return (
+
+    <>
       <h1 className='heading'>Hello, React!</h1>
       <h1> My name is {name}</h1>
-    </div>
+      <h1>{a + b}</h1>
+      <h2>{message()}</h2>
+
+      <div>
+        <table>
+          <Row />
+          <Column />
+        </table>
+      </div>
+    </>
   );
 }
+
+function Row() {
+  return (
+    <div>
+      <tr>
+        <th>Name</th>
+        <th>Address</th>
+      </tr>
+    </div>
+  )
+}
+function Column() {
+  return (
+    <div>
+      <tr>
+        <td>Name</td>
+        <td>Address</td>
+      </tr>
+    </div>
+  )
+}
+
+
 
 export default App;
