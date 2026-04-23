2617f23d351114cf73a9875c19e1f198445bbac3
jasirpk <jasijasu959@gmail.com>
2026-04-23 23:17:12 +0530

    inline css and variable throug css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import "./Header.css";
 
 const Header = () => {
+  const headingStyle = {color: "blue"};
   return (
-  <div>
-    <h1>Component 1</h1>
+  <div className='div-header'>
+    <h1 style={{color: "red"}}>Component 1</h1>
+    <h1 style={headingStyle}>Componet 2</h1>
+    <h2 className='header'>Paragraph Header</h2>
+    <p className='para'>This is a paragraph line</p>
   </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Child from './Child';
+import "./Home.css";
 
 const Home = () => {
     return (
-        <div>
+        <div className='Header'>
             <h1>Home Component</h1>
             <Child />
         </div>
