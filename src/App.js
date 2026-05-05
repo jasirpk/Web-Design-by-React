42c165cc817d42cc9c30c9f9833c60d5191556cf
jasirpk <jasijasu959@gmail.com>
2026-05-05 21:31:26 +0530

    api fetch completed

--- src/App.js
+++ src/App.js
@@ -1,81 +1,45 @@
 import './App.css';
 import React from 'react';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 // import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import States from './components/States';
+import States from './components/States/States';
 import { Container } from 'react-bootstrap';
-import Keys from './components/Keys';
+import Keys from './components/Keys/Keys';
 import Products from './components/Proudcts/Products';
-import Effects from './components/Effects';
+import Effects from './components/Effects/Effects';
+import { useEffect, useState } from 'react';
 
 
 
 function App() {
-  const name = 'john';
-  const a = 5;
-  const b = 10;
-  const message = () => {
-    return "Welcome to React!";
-  }
-  const states = [
-    { name: "Kerala", language: "Malayalam", Population: "1000" },
-    { name: "Tamil Nadu", language: "Tamil", Population: "10000" },
-    { name: "Karnataka", language: "Kannada", Population: "100000" }
-  ]
+
+  const [posts, setPosts] = useState([]);
+
+
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => setPosts(posts.splice(0, 5)))
+  }, [])
   return (
-
-    // <>
-    //   <h1 className='heading'>Hello, React!</h1>
-    //   <h1> My name is {name}</h1>
-    //   <h1>{a + b}</h1>
-    //   <h2>{message()}</h2>
-
-    //   <div className='appBetween'>
-    //     <table>
-    //       <Row />
-    //       <Column />
-    //      { /* component from header.js */}
-    //       <Header/>
-    //       {/* <Home/> */}
-    //      <Container>
-    //       <States states= {states} />
-    //      </Container>
-    //      <Keys />
-    //     </table>
-    //   </div>
-    // </> 
 
     <div className='App'>
-      <Container>
-        <Products />
-        <Effects />
-      </Container>
+      <h1>Fetch Api Data </h1>
+
+      {
+        posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))
+      }
     </div>
+
+
   );
 }
-
-function Row() {
-  return (
-    <div>
-      <tr>
-        <th>Name</th>
-        <th>Address</th>
-      </tr>
-    </div>
-  )
-}
-function Column() {
-  return (
-    <div>
-      <tr>
-        <td>Name</td>
-        <td>Address</td>
-      </tr>
-    </div>
-  )
-}
-
-
 
 export default App;
